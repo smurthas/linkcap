169836d5a8ae750850ed49a0961ca2198256762b
Simon Murtha-Smith <simon@murtha-smith.com>
2011-09-12 22:44:33 -0700

    mkdir -p on ~/Library/Services and don't add a DB_USER to .profile if it's already set

--- install.sh
+++ install.sh
@@ -19,12 +19,15 @@ if [ -z "${db}" ] && [ -z "$DB_USER" ]; then
     exit 1
 fi
 
-echo "" >> ~/.profile
-echo "export DB_USER=${db}" >> ~/.profile
+if [ -n "$DB_USER" ]; then
+    echo "" >> ~/.profile
+    echo "export DB_USER=${db}" >> ~/.profile
+fi
 
 if [ -n "${lgn}" ] && [ -n "${lgn}" ]; then
     echo "export BITLY_LOGIN=${lgn}" >> ~/.profile
     echo "export BITLY_API_KEY=${apik}" >> ~/.profile
 fi
 
+mkdir -p ~/Library/Services/
 cp -r LinkCap.workflow/ ~/Library/Services/LinkCap.workflow/
